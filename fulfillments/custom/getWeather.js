22188bd7ca3ac0331589218d78b45fff36d623f3
Berend Kalberg <berend.kalberg@gmail.com>
2020-09-25 15:52:15 +0200

    Can not specify time

--- fulfillments/custom/getWeather.js
+++ fulfillments/custom/getWeather.js
@@ -11,6 +11,9 @@ module.exports = {
         // Retrieves city parameter from Dialogflow
         // If a city has been entered that cannot be found in the api the application crashes
         var oldCity = agent.parameters['geo-city'];
+        // Retrieves date from Dialogflow
+        var dateString = agent.parameters['date']
+        var timeString = agent.parameters['time']
 
         // Log city to console for debugging
         console.log(oldCity)
@@ -22,29 +25,38 @@ module.exports = {
             .replace(/[óöôòõœøō]/g, 'o')
             .replace(/[éëêèęėē]/g, 'e')
 
-        // Retrieves date from Dialogflow
-        var dateString = agent.parameters['date']
+       
 
         // Log date to console for debugging
-        console.log(dateString)
+        console.log("dialogflow: " + dateString)
 
         // dateString to Date object places in variable date
         var date = new Date(dateString)
 
+        var time = new Date(timeString)
+
         // Log date to console for debugging
-        console.log(date)
+        console.log("converted: " + date)
 
         // Retrieves month from date
         var month = date.getMonth()
 
         // Log month to console for debugging
-        console.log(month)
+        console.log("month: " + month)
 
         // Retrieves day from date
         var day = date.getDate()
 
         // Log day to console for debugging
-        console.log(day)
+        console.log("day: " + day)
+
+        var hour = time.getHours()
+
+        console.log("hour: " + hour)
+
+        var time = date.getTime()
+
+        console.log(time / 1000)
 
         // Constant variable with my api key
         const apiKey = '459cf5b03efcfd9a0f668270e083ee99'
@@ -53,7 +65,7 @@ module.exports = {
         const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=nl&units=metric`
 
         // Log url for debugging
-        // console.log(url)
+        console.log(url)
 
         // Request made to url (api)
         // The api contains the weather for the next 5 days with intervals every 3 hours
@@ -78,6 +90,9 @@ module.exports = {
             // Log list from obj to debug retrieved data from api
             // Gives a lot of data so better comment it out unless actively debugging
             // console.log(obj.list)
+            for (var i = 0; i < obj.list.length; i++) {
+                console.log(obj.list[i].dt)
+            }
 
             // Log list length for debugging
             console.log('total list length: ' + obj.list.length)
@@ -89,21 +104,26 @@ module.exports = {
             for (var i = 0; i < obj.list.length; i++) {
                 // The api contains dt (date) which is in milliseconds so our date (in seconds) has to be converted to this format (* 1000)
                 var date = new Date(obj.list[i].dt * 1000)
+                console.log(date)
                 var weather = obj.list[i].weather[0].description
                 let temp = obj.list[i].main.temp
+                let dateText = obj.list[i].dt_txt
+
+                var correctedHours = date.getHours() - 2 
 
                 // Log month and day with their list number to console for debugging
-                // console.log(i + ":" + ' month is: ' + date.getMonth())
-                // console.log(i + ":" + ' day is: ' + date.getDate())
+                console.log(i + ":" + ' month is: ' + date.getMonth())
+                console.log(i + ":" + ' day is: ' + date.getDate())
+                console.log(i + ":" + ' hour is: ' + date.getHours())
 
                 // If month and day match user input description will update to the expected weather for that day
-                if (date.getMonth() == month && date.getDate() == day) {
-                    description = weather + " verwacht met een temperatuur van " + temp + " graden op " + date.toString()
+                if (date.getMonth() == month && date.getDate() == day && correctedHours == hour) {
+                    description = weather + " verwacht met een temperatuur van " + temp + " graden op " + dateText
+                    console.log(obj.list[i])
                 }
             }
             // Send description back to Dialogflow
             await agent.add(description)
-        agent.add("Hallo Test")
     }
 
 }
